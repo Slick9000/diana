49fdbb63cf133ec0fa22980e72e35a92bb94d0f7
Slick9000 <37941291+Slick9000@users.noreply.github.com>
2022-05-11 17:09:08 -0400

    split function addition

--- diana.lua
+++ diana.lua
@@ -129,4 +129,24 @@ function diana.question(prompt, valid)
 
 end
 
+function diana.split(inputstr, sep)
+     
+   if sep == nil then
+    
+      sep = "%s"
+    
+   end
+  
+   local t={}
+  
+   for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    
+      table.insert(t, str)
+    
+   end
+  
+   return t
+  
+end
+
 return diana
